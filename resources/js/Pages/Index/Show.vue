392ad624722b188159dca40cc19fa027f65e299e
Ed <eduramq@gmail.com>
2023-06-29 19:03:35 -0700

    Added common layout

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,9 +1,12 @@
 <template>
+<MainLayout>
     <div>Show</div>
     <Link href="/">Main Page</Link>
+</MainLayout>
 </template>
 
 
 <script setup>
 import {Link} from '@inertiajs/inertia-vue3'
+import MainLayout from '../../Layouts/MainLayout.vue';
 </script>
