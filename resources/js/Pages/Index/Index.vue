f931e06001bfbe8935db9e86d3b7e75db4797062
Ed <eduramq@gmail.com>
2023-06-29 20:32:32 -0700

    Set the default layout

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,11 +1,9 @@
 <template>
-
     <div>Index</div>
     <Link href="/hello">Show Page</Link>
     <div>
         The message is {{ message }}
     </div>
-
 </template>
 
 <script setup>
@@ -14,11 +12,4 @@ import {Link} from '@inertiajs/inertia-vue3'
 defineProps({
     message: String
 });
-</script>
-
-<script>
-import MainLayout from '../../Layouts/MainLayout.vue';
-export default{
-        layout: MainLayout
-    };
 </script>
