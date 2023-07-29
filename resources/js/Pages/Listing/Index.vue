13b62df5c27b273bc109f90f6e4489249eb1b9d2
Ed <eduramq@gmail.com>
2023-07-29 16:04:48 -0700

    Added delete method

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -5,7 +5,8 @@
         <ListingAddress :listing="listing" />
       </Link>
     </div>
-    <Link :href="`/listing/${listing.id}/edit`"> Edit </Link>
+    <Link :href="`/listing/${listing.id}/edit`" as="button"> Edit </Link>
+    <Link :href="`/listing/${listing.id}`" method="DELETE" as="button">Delete</Link>
   </div>
 </template>
 
