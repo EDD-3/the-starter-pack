c262a156d6b81d2ecd82881af514cfd757ba8309
Ed <eduramq@gmail.com>
2023-07-29 18:14:24 -0700

    Added components and tailwind styling

---
+++ resources/js/Components/UI/Box.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="border border-gray-200 rounded-md p-4 shadow-sm dark:border-gray-800 dark:text-gray-300">
+    <slot />
+  </div>
+</template>
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,12 +1,26 @@
 <template>
-  <Link href="/listing">Listings</Link>&nbsp;
-  <Link href="/listing/create">New Listing</Link>
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full"> 
+    <div class="container mx-auto"> 
+      <nav class="p-4 flex items-center justify-between">
+        <div class="text-lg font-medium">
+          <Link href="route('listing.index')">Listings</Link>&nbsp;
+        </div>
+        <div class="text-xl text-indigo-900 dark:text-indigo-600 ">  
+          <Link href="route('listing.index')">LaraZillow</Link>
+        </div>
+        <div>          
+          <Link href="route('listing.create') " class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing</Link>
+        </div>
+      </nav>
+    </div>
+  </header>
 
-  <div v-if="flashSuccess" class="success">
-    {{ flashSuccess }}
-  </div>
-  <!-- <div>The page with {{ timer }}</div> -->
-  <slot>Default</slot>
+  <main class="container mx-auto p-4">
+    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm bprder-green-200 bg-green-50 p-2 dark:border-green-800 dark:bg-green-800">
+      {{ flashSuccess }}
+    </div>
+    <slot>Default</slot>
+  </main>
 </template>
 
 <script setup>
